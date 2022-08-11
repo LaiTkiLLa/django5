5da7ac343892a2a02b85110d4fd32882a4b2ff7d
LaiTkiLLa <84670285+LaiTkiLLa@users.noreply.github.com>
2022-08-11 11:35:35 +0300

    Update serializers.py

--- api_with_restrictions/advertisements/serializers.py
+++ api_with_restrictions/advertisements/serializers.py
@@ -29,9 +29,6 @@ class AdvertisementSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Метод для создания"""
         
-        if Advertisement.objects.filter(creator=self.context["request"].user).filter(status='OPEN').count() > 10:
-            raise serializers.ValidationError('У вас больше 10 открытых объявлений')
-
         # Простановка значения поля создатель по-умолчанию.
         # Текущий пользователь является создателем объявления
         # изменить или переопределить его через API нельзя.
@@ -41,10 +38,17 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
 
-    def validate(self, data):
+   def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
-      
+        if self.context['request'].method == 'POST':
+            if Advertisement.objects.filter(creator=self.context["request"].user).filter(status='OPEN').count() > 10:
+                raise serializers.ValidationError('У вас больше 10 открытых объявлений')
+
+        if data.get('status') == 'OPEN':
+            if Advertisement.objects.filter(creator=self.context["request"].user).filter(status='OPEN').count() > 10:
+                raise serializers.ValidationError('У вас больше 10 открытых объявлений')
+
         # TODO: добавьте требуемую валидацию
 
         return data
